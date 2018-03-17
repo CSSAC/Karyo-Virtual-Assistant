f47c1331779786624faf218c8d14bd6cf6e781fc
prrateekk <prrateekk@hotmail.com>
2018-03-17 17:51:30 +0530

    first commit

---
+++ app/src/main/java/org/cssac/karyovirtualassistantforautistickids/models/UserInformation.java
@@ -0,0 +1,16 @@
+package org.cssac.karyovirtualassistantforautistickids.models;
+
+/**
+ * Created by prrateekk on 17/3/18.
+ */
+
+public class UserInformation {
+
+    public String firstName;
+    public String lastName;
+    public int age;
+
+    public UserInformation() {
+
+    }
+}
