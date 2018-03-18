f0238ea4c42697f47148353bae096ede1926329b
prrateekk <prrateekk@hotmail.com>
2018-03-18 23:52:05 +0530

    Added Delay and Retry Screen and mcq Load Screen

--- app/src/main/java/org/cssac/karyovirtualassistantforautistickids/MCQGameActivity.java
+++ app/src/main/java/org/cssac/karyovirtualassistantforautistickids/MCQGameActivity.java
@@ -1,7 +1,10 @@
 package org.cssac.karyovirtualassistantforautistickids;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,12 +21,21 @@ import java.util.List;
 
 
 public class MCQGameActivity extends AppCompatActivity {
-    private TextToSpeechModule ttsm;
     private static final String DRAWABLE = "drawable/";
+    private static final String EMPTY_STRING = "";
+    private static final String LIST_MCQ = "LIST_MCQ";
+    private static final String ID_OPTION_1 = "idOption1";
+    private static final String ID_OPTION_2 = "idOption2";
+    private static final String ID_OPTION_3 = "idOption3";
+    private static final int LOAD_SCREEN_DELAY = 5000;
+    private static final int SPEECH_DELAY = 500;
+    private static final int SET_OPTIONS_DELAY = 1000;
+    private static final int RETRY_SCREEN_DELAY = 1000;
 
     TextView idStatement;
     ImageView idOption1, idOption2, idOption3;
     private Drawable drawable;
+    Dialog loadScreenDialog, retryScreenDialog;
 
     List<MCQProblem> mcqProblemList;
     int mcqCounter;
@@ -31,11 +43,18 @@ public class MCQGameActivity extends AppCompatActivity {
 
     Animation animationZoomIn;
     Animation animationZoomOut;
+    TextToSpeechModule ttsm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mcqgame);
+
+        // Dialog Show
+        // Load data
+        // post delay - close dialog
+
+        showLoadScreen();
 
         idStatement = (TextView) findViewById(R.id.idStatement);
         idOption1 = (ImageView) findViewById(R.id.idOption1);
@@ -46,10 +65,10 @@ public class MCQGameActivity extends AppCompatActivity {
         animationZoomOut = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_out);
 
         Intent intent = getIntent();
-        mcqProblemList = (List<MCQProblem>) intent.getSerializableExtra("LIST_MCQ");
+        mcqProblemList = (List<MCQProblem>) intent.getSerializableExtra(LIST_MCQ);
 
+        speak(EMPTY_STRING);
         mcqCounter = 0;
-        nextMCQ();
 
         idOption1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,7 +78,7 @@ public class MCQGameActivity extends AppCompatActivity {
                 idOption1.startAnimation(animationZoomOut);
 
                 idStatement.setText(correctImage + "idOption1");
-                if (correctImage=="idOption1") nextMCQ();
+                if (correctImage==ID_OPTION_1) nextMCQ();
             }
         });
 
@@ -69,8 +88,9 @@ public class MCQGameActivity extends AppCompatActivity {
                 Log.i("ID", "idOption2");
                 idOption2.startAnimation(animationZoomIn);
                 idOption2.startAnimation(animationZoomOut);
+
                 idStatement.setText(correctImage + "idOption2");
-                if (correctImage=="idOption2") nextMCQ();
+                if (correctImage==ID_OPTION_2) nextMCQ();
             }
         });
 
@@ -80,82 +100,138 @@ public class MCQGameActivity extends AppCompatActivity {
                 Log.i("ID", "idOption3");
                 idOption3.startAnimation(animationZoomIn);
                 idOption3.startAnimation(animationZoomOut);
+
                 idStatement.setText(correctImage + "idOption3");
-                if (correctImage=="idOption3") nextMCQ();
+                if (correctImage==ID_OPTION_3) nextMCQ();
             }
         });
 
+        setOptions();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadScreenDialog.dismiss();
+                speakAfterDelay();
+            }
+        }, LOAD_SCREEN_DELAY);
+    }
+
+    public void showLoadScreen() {
+        View view = getLayoutInflater().inflate(R.layout.load_game_screen, null);
+        loadScreenDialog = new Dialog(this, R.style.MyTheme);
+        loadScreenDialog.setContentView(view);
+        loadScreenDialog.setCancelable(false);
+        loadScreenDialog.show();
+    }
+
+    public void showRetryScreen() {
+        View view = getLayoutInflater().inflate(R.layout.retry_mcq, null);
+        retryScreenDialog = new Dialog(this, R.style.MyTheme);
+        retryScreenDialog.setContentView(view);
+        retryScreenDialog.setCancelable(false);
+        retryScreenDialog.show();
+    }
+
+    public void speak(String text) {
+        ttsm = new TextToSpeechModule(text, this);
+    }
+
+    public void speakAfterDelay() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                speak(mcqProblemList.get(mcqCounter).getStatement());
+                mcqCounter++;
+            }
+        }, SPEECH_DELAY);
+    }
+
+    public void setOptions() {
+        String options[] = mcqProblemList.get(mcqCounter).getOptions();
+        String correctAns = mcqProblemList.get(mcqCounter).getCorrectAnswer();
+        idStatement.setText(mcqProblemList.get(mcqCounter).getStatement() + correctAns);
+
+        int dr = getResources().getIdentifier(DRAWABLE + options[0], null, getPackageName());
+        drawable = getResources().getDrawable(dr);
+        idOption1.setImageDrawable(drawable);
+
+        dr = getResources().getIdentifier(DRAWABLE + options[1], null, getPackageName());
+        drawable = getResources().getDrawable(dr);
+        idOption2.setImageDrawable(drawable);
+
+        dr = getResources().getIdentifier(DRAWABLE + options[2], null, getPackageName());
+        drawable = getResources().getDrawable(dr);
+        idOption3.setImageDrawable(drawable);
+
+        if (options[0].equals(correctAns)) {
+            correctImage = ID_OPTION_1;
+        }
+        else if (options[1].equals(correctAns)) {
+            correctImage = ID_OPTION_2;
+        }
+        else if (options[2].equals(correctAns)){
+            correctImage = ID_OPTION_3;
+        }
+
+        idStatement.setText(correctAns + correctImage + " " + options[0] + options[1] + options[2]);
+    }
+
+    public void setOptionsAfterDelay() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setOptions();
+                speakAfterDelay();
+            }
+        }, SET_OPTIONS_DELAY);
     }
 
     public void nextMCQ() {
         Log.i("TAG", "NEXT MCQ");
         if (mcqCounter<mcqProblemList.size()) {
             Log.i("TAG", "IN RANGE");
-            String options[] = mcqProblemList.get(mcqCounter).getOptions();
-            String correctAns = mcqProblemList.get(mcqCounter).getCorrectAnswer();
-            idStatement.setText(mcqProblemList.get(mcqCounter).getStatement() + correctAns);
-
-            int dr = getResources().getIdentifier(DRAWABLE + options[0], null, getPackageName());
-            drawable = getResources().getDrawable(dr);
-            idOption1.setImageDrawable(drawable);
-
-            dr = getResources().getIdentifier(DRAWABLE + options[1], null, getPackageName());
-            drawable = getResources().getDrawable(dr);
-            idOption2.setImageDrawable(drawable);
-
-            dr = getResources().getIdentifier(DRAWABLE + options[2], null, getPackageName());
-            drawable = getResources().getDrawable(dr);
-            idOption3.setImageDrawable(drawable);
-
-            if (options[0].equals(correctAns)) {
-                correctImage = "idOption1";
-            }
-            else if (options[1].equals(correctAns)) {
-                correctImage = "idOption2";
-            }
-            else if (options[2].equals(correctAns)){
-                correctImage = "idOption3";
-            }
-
-            idStatement.setText(correctAns + correctImage + " " + options[0] + options[1] + options[2]);
-
-            ttsm = new TextToSpeechModule(mcqProblemList.get(mcqCounter).getStatement(), this);
-            mcqCounter++;
+            setOptionsAfterDelay();
+        }
+        else {
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    showRetryScreen();
+                }
+            }, RETRY_SCREEN_DELAY);
         }
     }
 
-    public void nextByButton(View view) {
-        Log.i("TAG", "NEXT MCQ");
-        if (mcqCounter<mcqProblemList.size()) {
-            Log.i("TAG", "IN RANGE");
-            String options[] = mcqProblemList.get(mcqCounter).getOptions();
-            idStatement.setText(mcqProblemList.get(mcqCounter).getStatement());
+    public void retryLevel(View v) {
+        mcqCounter = 0;
+        setOptions();
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // SAVE GAME DATA
 
-            int dr = getResources().getIdentifier(DRAWABLE + options[0], null, getPackageName());
-            drawable = getResources().getDrawable(dr);
-            idOption1.setImageDrawable(drawable);
-
-            dr = getResources().getIdentifier(DRAWABLE + options[1], null, getPackageName());
-            drawable = getResources().getDrawable(dr);
-            idOption2.setImageDrawable(drawable);
-
-            dr = getResources().getIdentifier(DRAWABLE + options[2], null, getPackageName());
-            drawable = getResources().getDrawable(dr);
-            idOption3.setImageDrawable(drawable);
-
-            String correctAns = mcqProblemList.get(mcqCounter).getCorrectAnswer();
-            if (options[0]==correctAns) {
-                correctImage = "idOption1";
+                retryScreenDialog.dismiss();
+                speakAfterDelay();
             }
-            else if (options[1]==correctAns) {
-                correctImage = "idOption2";
-            }
-            else {
-                correctImage = "idOption3";
-            }
+        }, SET_OPTIONS_DELAY);
+    }
 
-            ttsm = new TextToSpeechModule(mcqProblemList.get(mcqCounter).getStatement(), this);
-            mcqCounter++;
-        }
+    public void backToMainMenu(View v) {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // SAVE GAME DATA
+
+                // finish current activity which takes us to un-finishied LearningAppHomeActivity
+                finish();
+            }
+        }, RETRY_SCREEN_DELAY);
     }
 }
